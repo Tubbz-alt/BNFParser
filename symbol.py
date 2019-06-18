482a0f1dfd44545b83a664ea0858bc4a1687c852
ngrahovac <ninagrahovac.96@gmail.com>
2019-06-18 16:16:46 +0200

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -5,27 +5,33 @@ import re
 class Symbol:
     def __init__(self, name, regex = "", prods = []):
         self.name = name
-        self.terminal = False
         self.open_tag = "<" + name + ">"                                        # will be overwritten for literals
         self.closed_tag = re.sub("<", "</", self.open_tag)
         self.regex = regex
-        self.prods = []                                                         # possible productions
+        self.prods = []
 
 
     def update_regex(self):
-        if (self.regex == ""):
-            if len(self.prods) == 1 and not self.terminal:                      # len(self.prods) == 0 for nonterms mapping to terms only
+        if self.regex == "":
+            if len(self.prods) == 1:
                 prod = self.prods[0]
-                prod.update_regex()
-                self.regex = prod.regex
+                if (prod.regex == ""):
+                    prod.update_regex()
+                self.regex += prod.regex
             elif len(self.prods) > 1:
                 self.regex += "("
                 for prod in self.prods:
                     if prod.regex == "":
                         prod.update_regex()
                     self.regex += prod.regex + "|"
+                if len(self.prods) > 0:
+                    self.regex = self.regex[0:-1]                                # eliminating the last |
+                self.regex += ")"
 
-                self.regex = self.regex[0:-1] + ")"                             # eliminating the last |
+
+    def print(self):
+        pass
+
 
 
 class Production:
@@ -35,14 +41,14 @@ class Production:
 
 
     def update_regex(self):
-        if (self.regex == ""):
+        if self.regex == "":
             self.regex += "("
             for symbol in self.symbols:
                 if symbol.regex == "":
                     symbol.update_regex()
                 self.regex += symbol.regex
-            self.regex += ")"
+            self.regex += ")"                                                   # extra () for a single nonterm in prod
 
 
-    def print_prod(self):
+    def print(self):
         pass
